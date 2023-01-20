d016b47c38150d6665cafe6af6ac0b88602db0fa
Luiz Vieira <luiz.vieira@jeitto.com.br>
2023-01-20 11:03:32 -0300

    database files

---
+++ src/top_10_ano.sql
@@ -0,0 +1,13 @@
+SELECT  o.registro_ans,
+        o.nome_fantasia,
+		o.razao_social,
+		ROUND(SUM(VL_SALDO_FINAL),2) as TOTAL 
+from operadoras_ativas o
+	LEFT JOIN demonstracoes_contabeis d ON o.registro_ans = d.REG_ANS
+WHERE d.ANO =  YEAR(DATE_SUB(CURDATE(), INTERVAL 1 YEAR))
+	AND d.DESCRICAO = 'EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR '
+GROUP BY o.registro_ans,
+		o.nome_fantasia,
+		o.razao_social
+ORDER BY SUM(VL_SALDO_FINAL) DESC
+limit 10;
---
+++ src/top_10_trimestre.sql
@@ -0,0 +1,14 @@
+SELECT  o.registro_ans,
+        o.nome_fantasia,
+		o.razao_social,
+		ROUND(SUM(VL_SALDO_FINAL),2) as TOTAL 
+from operadoras_ativas o
+	LEFT JOIN demonstracoes_contabeis d ON o.registro_ans = d.REG_ANS
+WHERE d.TRIMESTRE =  QUARTER(DATE_SUB(CURDATE(), INTERVAL 2 QUARTER))
+	AND d.ANO =  YEAR(DATE_SUB(CURDATE(), INTERVAL 2 QUARTER))
+	AND d.DESCRICAO = 'EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR '
+GROUP BY o.registro_ans,
+		o.nome_fantasia,
+		o.razao_social
+ORDER BY SUM(VL_SALDO_FINAL) DESC
+limit 10;
